900bf593a1c6218616b4e7620e0b65d352f1cb34
Vijay Vepakomma <vijay.vepakomma@philips.com>
2018-02-03 11:19:57 -0500

    Example code created from template

---
+++ UrhoGameExample1/UrhoGameExample1/UrhoGameExample1.Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿using Urho;
+
+namespace UrhoGameExample1.Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MyGame(new ApplicationOptions("Data")).Run();
+        }
+    }
+}
